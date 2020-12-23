57ad4f1ad43e7fb28691cfd189b5ff7f57621dbd
Ali Ejlalzadeh <aliejlalzadeh1379@gmail.com>
2020-12-23 16:01:22 +0330

    binary search completed

--- binary_search.py
+++ binary_search.py
@@ -3,12 +3,12 @@ target= int (input("Enter your target number:"))
 lst=[int(i) for i in inp_str.split(' ') ]
 
 def binary_search(lst,low,high,target):
-    if low <high:
+    if low <=high:
         mid = (low +high)//2
         if lst[mid]==target:
             return mid+1
         elif lst[mid]>target:
-            return binary_search(lst,0,mid-1,target)
+            return binary_search(lst,low,mid-1,target)
         elif lst[mid]<target:
             return binary_search(lst,mid+1,high,target)
     else : 
