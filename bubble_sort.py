9a458d7e4b8ee2ab6e278a408155e40b6580787d
Ali Ejlalzadeh <aliejlalzadeh1379@gmail.com>
2020-12-25 00:28:33 +0330

    bubble sort added

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+inp_str = input("Enter numbers:")
+lst=[int(i) for i in inp_str.split(' ') ]
+
+def bubble_sort(lst):
+    for i in range(0,len(lst)-1):
+        for j in range(0,len(lst)-i-1):
+            if lst[j]>lst[j+1]:
+                lst[j],lst[j+1]=lst[j+1],lst[j]
+
+
+bubble_sort(lst)
+print(lst)                
---
+++ quick_sort.py
@@ -0,0 +1,5 @@
+inp_str = input("Enter numbers:")
+lst=[int(i) for i in inp_str.split(' ') ]
+def quick_sort(lst,low ,high):
+    if low<=high:
+        pivot
